2653a4a639be9dd6c781a339228c520f907fcca6
Wilfried Hortschitz <wilfried.hortschitz@donau-uni.ac.at>
2025-04-30 14:39:18 +0200

    add ifw parser and schema 	new file:   ifwparser.py 	new file:   ../schema_packages/ifw_schema.py

---
+++ src/cube/parsers/ifwparser.py
@@ -0,0 +1,73 @@
+import os
+
+from nomad.datamodel import EntryArchive
+from nomad.parsing import MatchingParser
+
+from cube.schema_packages.ifw_schema import IFWData
+
+
+class IFWParser(MatchingParser):
+  def is_mainfile(
+    self,
+    filename: str,
+    mime: str,
+    buffer: bytes,
+    decoded_buffer: str,
+    compression: str = None,
+  ):
+    print(f'filename {filename}, mime {mime}, compression {compression}')
+
+    if os.path.basename(filename) != "VSM_M(H).json":
+      return False
+
+    return True
+  
+
+  def parse(
+      self,
+      mainfile: str,
+      archive: EntryArchive,
+      logger=None,
+      child_archives: dict[str, EntryArchive] = None,
+    ) -> None:
+      print(f'mainfile {mainfile}, archive {archive}, child_archives {child_archives}')
+      logger.info('IFWParser called')
+
+      baseDir = os.path.dirname(mainfile)
+      idx = baseDir.rfind('raw/')
+      if idx == -1:
+        archiveBaseDir = baseDir
+      else:
+        archiveBaseDir = baseDir[idx + 4:]
+
+      # data_dir_GS = baseDir + "/GS/"
+      # archiveData_dir_GS = archiveBaseDir + "/GS/"
+      # data_dir_MC = baseDir + "/MC"
+
+      # xyz_dirs = [dirdir for dirdir in os.listdir(data_dir_GS) if len(dirdir) == 1]
+
+      # print(f'data_dir_GS {data_dir_GS} data_dir_MC {data_dir_MC}'
+      #        f' xyz_dirs {xyz_dirs} idx{idx}')
+
+      # # Reading file into lines; all folders are equivalent according to 
+      # # UU-colleagues, so we can use the first one
+      # file_Name_Ms = archiveBaseDir + "/GS/" + f"{xyz_dirs[0]}/out_last"
+      # print(f'file_Name_Ms {file_Name_Ms}')
+
+      # fx = archiveData_dir_GS + "x/out_MF_x" if 'x' in xyz_dirs else None
+      # fy = archiveData_dir_GS + "y/out_MF_y" if 'y' in xyz_dirs else None
+      # fz = archiveData_dir_GS + "z/out_MF_z" if 'z' in xyz_dirs else None
+      # fol = f"{archiveData_dir_GS}{xyz_dirs[0]}/out_last"
+
+      # # entry = Cube(data_file=file)
+      # groundState = GroundState(out_MF_x=fx,out_MF_y=fy,out_MF_z=fz)
+      # # groundState.normalize(archive=archive,logger=logger)
+      # entry = UUData(groundState=groundState,out_last_file=fol)
+      
+      
+      MoH = f"{archiveBaseDir}/VSM_M(H).json"
+      print(f'MoH {MoH}')
+
+      entry = IFWData(M_of_H_file=MoH)
+      
+      archive.data = entry
---
+++ src/cube/schema_packages/ifw_schema.py
@@ -0,0 +1,71 @@
+from typing import (
+  TYPE_CHECKING,
+)
+
+import json
+
+from nomad.datamodel.data import (
+  ArchiveSection,
+  EntryData,
+)
+from nomad.metainfo import (
+  Package,
+  Quantity,
+  Section,
+  SubSection,
+)
+from nomad.units import ureg
+
+from .mammos_ontology import MagnetocrystallineAnisotropyConstantK1, SpontaneousMagneticPolarisation, MaximumEnergyProduct
+
+if TYPE_CHECKING:
+  from nomad.datamodel.datamodel import (
+      EntryArchive,
+  )
+  from structlog.stdlib import (
+      BoundLogger,
+  )
+
+m_package = Package(name='Schema for IFW data')
+m_package.__init_metainfo__()
+
+
+
+class IFWData(EntryData, ArchiveSection):
+  m_def = Section()
+
+  BHmax = SubSection(
+    section_def=MaximumEnergyProduct,
+    repeats = False,
+  )
+
+
+  M_of_H_file = Quantity(
+    type=str,
+    description='The \'VSM_M(H)\' file.',
+    a_eln={
+        "component": "FileEditQuantity",
+    },
+  )
+
+  def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+    '''
+    The normalizer for the `IFW data`.
+
+    Args:
+        archive (EntryArchive): The archive containing the section that is being
+        normalized.
+        logger (BoundLogger): A structlog logger.
+    '''
+    super().normalize(archive, logger)
+
+    if self.M_of_H_file is not None:
+        print(f'Normalizing IFW data: {self.M_of_H_file}')
+        # print(archive.m_context.raw_file(self.M_of_H_file))
+        with archive.m_context.raw_file(self.M_of_H_file) as file:
+            # content = json.load(file)
+            content = json.loads(file.read())
+
+        self.BHmax = MaximumEnergyProduct
+        self.BHmax.MaximumEnergyProduct = \
+            ureg.Quantity(float(content['BHmax in kJ/m^3'][0], 'kJ/m^3'))
